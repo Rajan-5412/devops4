4cf36c72503823b1cac0553e1ac531462dc52fa4
Rajan-91480 <rajan91480@gmail.com>
2026-06-20 13:45:02 +0530

    1st commit after building a java application and run the maven project

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    static void main() {
+        System.out.println("Hello students");
+        System.out.println("Welcome to Devops Lab");
+        System.out.println("Lab Program no.4");
+    }
+}
